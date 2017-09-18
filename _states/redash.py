0327e73e817e1941c6cc7dbbe9beaa69d1b7f50f
J. Eduardo <j.eduardo@gmail.com>
2017-09-19 00:35:13 +0200

    Fixed datasources_present state to work for creating and updating new data sources.

--- _states/redash.py
+++ _states/redash.py
@@ -6,6 +6,7 @@ Interact with Redash.
 https://redash.io
 '''
 
+import collections
 import logging
 
 # Define the module's virtual name
@@ -14,39 +15,59 @@ __virtualname__ = 'redash'
 log = logging.getLogger(__name__)
 
 
-def datasource_present(name, type, options):
+def _compare_hashes(hash1, hash2, filter=[]):
+    log.debug('Hash1: %s' % hash1)
+    log.debug('Hash2: %s' % hash2)
+    if len(hash1.keys()) != len(hash2.keys()):
+        log.debug('Hashes have different number of keys')
+        return False
+    for key in hash1.keys():
+        if key in filter:
+            continue
+        if key not in hash2.keys():
+            log.debug('Key %s not found in hash2' % key)
+            return False
+        log.debug('Comparing key %s: %s - %s' % (key, hash1[key], hash2[key]))
+        if hash1[key] != hash2[key]:
+            return False
+    log.debug('Hashes considered equal')
+    return True
+
+def datasource_present(name, type, options, force=False):
     ret = {'name': name,
            'changes': {},
            'result': False,
            'comment': ''}
-    update = False
-    # Check if datasource exists
-    datasources = __salt__['redash.list_datasources']()
-    existing_ds = datasources.get(name)
-    if existing_ds \
-            and existing_ds['type'] == type \
-            and existing_ds['options'] == options:
-        # No changes in datasource
-        # WARNING: this code path may be always dead as Redash seems to be 
-        # censoring the returning password.
-        ret['result'] = True
-        ret['comment'] = 'The datasource "%s" is already present.' % name
-        return ret
-    else:
-        update = True
 
-    # Create the datasource if not present
-    if not update:
-        if (__salt__['redash.add_datasource'](name=name, type=type,
-                                              options=options)):
+    res = __salt__['redash.list_datasources'](name=name)
+    # Check if datasource exists
+    if name in res.keys():
+        existing_ds = res[name]
+        # Check whether the existing datasource is different
+        # than the defintion
+        log.debug('Datasource info for %s: %s' % (name, existing_ds))
+        existing_options = existing_ds['options']
+        if force or type != existing_ds['type'] \
+                or not _compare_hashes(options, existing_options,
+                                       filter=['password']):
+            # Need to update the DS
+            updated_ds = __salt__['redash.alter_datasource'](
+                id=existing_ds['id'],
+                name=name, type=type,
+                options=options)
             ret['result'] = True
-            ret['comment'] = 'The datasource "%s" was created' % name
-            # ret['changes'] = { name : { 'old': '', 'new': zone } }
+            ret['comment'] = 'Datasource was updated'
+            ret['changes'] = {name: {'old': existing_ds, 'new': updated_ds}}
+        else:
+            ret['result'] = True
+            ret['comment'] = 'Datasource is present'
     else:
-        if (__salt__['redash.alter_datasource'](id=existing_ds['id'],
-                                                name=name, type=type,
-                                                options=options)):
+        # Create the datasource if not present
+        new_ds = __salt__['redash.add_datasource'](name=name, type=type,
+                                                   options=options)
+        if (new_ds):
             ret['result'] = True
-            ret['comment'] = 'The datasource "%s" was updated' % name
+            ret['comment'] = 'Datasource created'
+            ret['changes'] = {name: {'old': '', 'new': new_ds}}
 
     return ret
